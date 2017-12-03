b5071d20bf1353ce037fdc7f4058a6ae3d16c3e5
Avinash Sridhar <sridharavinash@github.com>
2017-12-02 19:17:07 -0500

    brute force n^2

--- nim/cksum.nim
+++ nim/cksum.nim
@@ -1,13 +1,24 @@
 import strutils
 import sequtils
 
+#for part 1
 proc diff(row: seq[int]): int =
   return (max(row) - min(row))
+
+#for part2
+proc modd(row: seq[int]): int =
+  for i,v1 in row:
+    for j,v2 in row:
+      if v1 == v2:
+        continue
+      if v1 mod v2 == 0:
+        return int(v1/v2)
+  
 
 proc cksum(f: string): int =
   var sum = 0
   for line in lines f:
-    sum += diff(map(line.split, parseInt))
+    sum += modd(map(line.split, parseInt))
   return sum
 
 echo cksum("cksumin.txt")
--- python/cksum.py
+++ python/cksum.py
@@ -1,12 +1,22 @@
 #!/usr/local/bin/python3
 
+#for part 1
 def diff(row):
     return abs(max(row) - min(row))
 
+#for part 2
+def mod(row):
+    for i in row:
+        for j in row:
+            if i == j:
+                continue
+            if i % j == 0:
+                return i/j
+            
 def cksum(filename):
     f = open(filename, 'r')
     cksum = 0
-    cksum = [diff([int(x) for x in line.split()]) for line in f]
+    cksum = [mod([int(x) for x in line.split()]) for line in f]
     return sum(cksum)
 
 print(cksum("cksumin.txt"))
