3350e79a577abcf15eac24a4c9d61b291b271b05
Avinash Sridhar <sridharavinash@github.com>
2017-12-04 14:43:50 -0500

    part 2 of phrases with anagram detection

--- python/passphrase.py
+++ python/passphrase.py
@@ -8,15 +8,29 @@ def testPhrase(phrase):
         wp.append(w)
     return True
 
+def testPhraseAnagram(phrase):
+    wp = []
+    for w in phrase.split():
+        s = ''.join(sorted(w))
+        if s in wp:
+            return False
+        wp.append(s)
+    return True
+
 
 assert True == testPhrase("aa bb cc dd ee")
 assert False == testPhrase("aa bb cc dd aa")
 assert True ==  testPhrase("aa bb cc dd aaa")
+assert True == testPhraseAnagram("abcde fghij")
+assert False == testPhraseAnagram("abcde fghij edcab")
 
 valid = 0
+v2=0
 for line in open("phraseinput.txt",'r'):
     if testPhrase(line):
         valid += 1
+    if testPhraseAnagram(line):
+        v2 += 1
 
 print(valid)
-        
+print(v2)        
