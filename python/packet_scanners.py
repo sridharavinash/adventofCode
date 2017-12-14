d09751f40aa68662b6dae36659ad622e2f25c939
Avinash Sridhar <sridharavinash@github.com>
2017-12-13 22:38:50 -0500

    day 13 part 2
    abandoned brute force to notice the property of periodic values for
    when the scanner gets to the top. :facepalm:

--- python/packet_scanners.py
+++ python/packet_scanners.py
@@ -1,5 +1,4 @@
 
-
 def move_scanner(s,state,d):
     for i in s:
         if d[i] == 'u':
@@ -12,9 +11,18 @@ def move_scanner(s,state,d):
             if ((state[i] - 1) < 0):
                 d[i] = 'd'
     return state,d
+
+def Caught(s,delay):
+    for i in s:
+        depth = s[i]
+        whenZero = (2*(depth-1))
+        if (i+delay) % whenZero == 0:
+            #print("caught:",i)
+            return True
+    print("delay:",delay)
+    return False  
     
-def moves(s):
-    print()
+def moves(s,delay=0):
     total_moves = max(s.keys())
     state = {}
     d = {}
@@ -22,25 +30,34 @@ def moves(s):
     for l in s:
         state[l] = 0
         d[l] = 'd'
-    #state,d = move_scanner(s,state,d)
-    print(state,pos)
+    for x in range(delay):
+        state,d = move_scanner(s,state,d)
+    print("delay:",delay,state)
     sev = 0
+    caught = False
     for i in range(total_moves+1):
         pos +=1
-        print("before:",state, pos)
+        #print("before:",state, pos)
         if (pos in s) and (state[pos] == 0):
             sev += s[pos] * pos
             print("caught:", pos,"sev:",sev)
+            caught = True
+            #return sev, caught
+            #print("caught:",state, pos)
         state,d = move_scanner(s,state,d)
-        print("after:",state, pos)
-    return sev
-        
-        
+    return sev,caught
+                
 
 scanner = {}
-for line in open("../packets_test.in"):
+for line in open("../packets_p.in"):
     layer,depth = line.split(':')
     scanner[int(layer)] = int(depth)
 
 print(scanner, max(scanner.keys()))
-print(moves(scanner))
+#print(moves(scanner,2))
+c = True
+delay = 0
+while c:
+    c = Caught(scanner,delay)
+    delay +=1
+
