765039db047042182fc7edc74df5c3d2285697c8
Avinash Sridhar <sridharavinash@github.com>
2017-12-18 15:40:03 -0500

    Day 18 Covert to class

--- python/day18.duet.py
+++ python/day18.duet.py
@@ -1,76 +1,85 @@
 import re
+import queue
 
 
-registers = {'end':False, 'jmp':0}
+class Program(object):
+    def __init__(self,infile):
+        self.registers = {'end':False, 'jmp':0}
+        self.q  = queue.Queue()
+        self.infile = infile
 
-def mul (x,y):
-    return x * y
+    def mul (self,x,y):
+        return x * y
 
-def snd(x):
-    x_prev = x +'_prev'
-    registers[x_prev] = registers[x]
-    return registers[x]
+    def snd(self,x):
+        x_prev = x +'_prev'
+        self.registers[x_prev] = self.registers[x]
+        return self.registers[x]
 
-def rcv(x):
-    prev = x + "_prev"
-    if prev not in registers:
-        return
-    if registers[x] > 0:
-        registers["end"] = True
-        print("rcv:",registers[prev])
+    def rcv(self,x):
+        prev = x + "_prev"
+        if prev not in self.registers:
+            return
+        if self.registers[x] > 0:
+            self.registers["end"] = True
+            print("rcv:",self.registers[prev])
 
-def jgz(reg,jmp):
-    if reg.isdigit():
-        if int(reg) > 0: return jmp
-    if registers[reg] > 0:
-        return jmp
-    return 0
+    def jgz(self,reg,jmp):
+        if reg.isdigit():
+            if int(reg) > 0: return jmp
+        if self.registers[reg] > 0:
+            return jmp
+        return 0
 
-def parse(line):
-    r = re.match(r"^(?P<ins>\w+) (?P<reg>\w+) ?(?P<val>.*)",line)
-    return r
+    def parse(self,line):
+        r = re.match(r"^(?P<ins>\w+) (?P<reg>\w+) ?(?P<val>.*)",line)
+        return r
 
-def parse_val(v):
-    if v in registers:
-        return int(registers[v])
-    else:
-        try:
-            return int(v, 10)
-        except ValueError:
-            return 0
+    def parse_val(self,v):
+        if v in self.registers:
+            return int(self.registers[v])
+        else:
+            try:
+                return int(v, 10)
+            except ValueError:
+                return 0
 
-def process(r):
-    ins = r.group('ins')
-    reg = r.group('reg')
-    v = r.group('val')
-    val = parse_val(v)
-    if ins == "set":
-        registers[reg] = val
-    if ins == "add":
-        if reg not in registers:
-            registers[reg] = 0
-        registers[reg] += val
-    if ins == "mul":
-        if reg not in registers:
-            registers[reg] = 0
-        registers[reg] *= val
-    if ins == "mod":
-        registers[reg] %= val
-    if ins == "snd":
-        snd(reg)
-    if ins == "rcv":
-        rcv(reg)
-    if ins == "jgz":
-       registers['jmp'] =  jgz(reg,val)
-    
-lines = open("../duet_p.in").readlines()
-i = 0
-while not registers["end"]:
-    r = parse(lines[i])
-    process(r)
-    if abs(registers['jmp']) > 0:
-        i += (registers['jmp'])
-        registers['jmp'] = 0
-    else:
-        i+=1
-    
+    def process(self,r):
+        ins = r.group('ins')
+        reg = r.group('reg')
+        v = r.group('val')
+        val = self.parse_val(v)
+        if ins == "set":
+            self.registers[reg] = val
+        if ins == "add":
+            if reg not in self.registers:
+                self.registers[reg] = 0
+            self.registers[reg] += val
+        if ins == "mul":
+            if reg not in self.registers:
+                self.registers[reg] = 0
+            self.registers[reg] *= val
+        if ins == "mod":
+            self.registers[reg] %= val
+        if ins == "snd":
+            self.snd(reg)
+        if ins == "rcv":
+            self.rcv(reg)
+        if ins == "jgz":
+           self.registers['jmp'] =  self.jgz(reg,val)
+
+    def run(self):
+        lines = open(self.infile).readlines()
+        i = 0
+        while not self.registers["end"]:
+            r = self.parse(lines[i])
+            self.process(r)
+            if abs(self.registers['jmp']) > 0:
+                i += (self.registers['jmp'])
+                self.registers['jmp'] = 0
+            else:
+                i+=1
+
+p = Program("../duet_p.in")
+p.run()
+
