dd313f9287234d01a6871e4fd1953c2f0de21d8d
Avinash Sridhar <sridharavinash@github.com>
2017-12-22 02:51:53 -0500

    part 2 of day 22

--- python/day22.sporifica.py
+++ python/day22.sporifica.py
@@ -15,7 +15,7 @@ def convertToPatternRow(row):
     return ['#' if x == 1 else '.' for x in row]
 
 inmat = parsePattern("../spvirus_p.in")
-inmat = np.pad(inmat,(192,192),'constant',constant_values=(0))
+inmat = np.pad(inmat,(384,384),'constant',constant_values=(0))
 x,y = inmat.shape
 cx= int(x/2)
 cy = int(y/2)
@@ -25,29 +25,60 @@ directions = {
     'r':{'l':(-1,0,'u'),'r':(1,0,'d')},
     'd':{'l':(0,1,'r'),'r':(0,-1,'l')}
 }
+
+directions_noturn = {
+    'u': (-1,0,'u'),
+    'd': (1,0,'d'),
+    'l': (0,-1,'l'),
+    'r': (0,1,'r')
+}
+
+directions_reverse = {
+    'u': (1,0,'d'),
+    'd': (-1,0,'u'),
+    'l': (0,1,'r'),
+    'r': (0,-1,'l')
+}
+
+
+states = {'c':0,'w':2,'i':1,'f':3}
 print()
 curr = 'u'
 infected = 0
-for i in range(10000):
+for i in range(10000000):
     #print(inmat)
-    print("infected:" , infected)
     #print(cx,cy)
     #input()
-    if inmat[cx,cy] == 1:
+    if inmat[cx,cy] == states['i']:
         #infected , turn right, move forward
-        #clean the node
+        #flag the node
         x,y,curr = directions[curr]['r']
-        inmat[cx,cy] = 0
+        inmat[cx,cy] = states['f']
         cx += x
         cy += y
-
-    else:
-        #clean node,turn left move forward
-        #infect the node
+        continue
+    if inmat[cx,cy] == states['c']:
+        #weaken  node,turn left move forward
         x,y,curr = directions[curr]['l']
-        infected += 1
-        inmat[cx,cy] = 1
+        inmat[cx,cy] = states['w']
         cx += x
         cy += y
+        continue
+    if inmat[cx,cy] == states['f']:
+        #flagged, reverse
+        x,y,curr = directions_reverse[curr]
+        inmat[cx,cy] = states['c']
+        cx += x
+        cy += y
+        continue
+    if inmat[cx,cy] == states['w']:
+        #weakened, no turn
+        #infect the node
+        x,y,curr = directions_noturn[curr]
+        inmat[cx,cy] = states['i']
+        infected += 1
+        cx += x
+        cy += y
+        continue
 
-    
+print("infected:" , infected)
