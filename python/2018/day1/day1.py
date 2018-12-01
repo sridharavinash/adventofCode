9068a16d361566a565e3445c28731489e795b88a
Avinash Sridhar <sridharavinash@github.com>
2018-12-01 11:24:06 -0500

    day 1 2018

---
+++ python/2018/day1/day1.py
@@ -0,0 +1,16 @@
+
+def main():
+    s = 0
+    freqs = []
+    while 1:
+        for line in open("input.txt","r"):
+            s += int(line)
+            if s in freqs:
+                print("repeated",s)
+                exit(0)
+                break
+            freqs.append(s)
+        print("sum",s)
+
+if __name__ == "__main__":
+    main()
