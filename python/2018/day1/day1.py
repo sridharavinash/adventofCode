c9cb27574474963fa9af045d0671cc4c77038326
Avinash Sridhar <sridharavinash@github.com>
2018-12-01 14:49:28 -0500

    faster dict comparison instead of slow linear lists

--- python/2018/day1/day1.py
+++ python/2018/day1/day1.py
@@ -1,14 +1,14 @@
 
 def main():
     s = 0
-    freqs = []
+    freqs = {}
     while 1:
         for line in open("input.txt","r"):
             s += int(line)
             if s in freqs:
                 print("repeated",s)
                 exit(0)
-            freqs.append(s)
+            freqs[s]=1
         print("sum",s)
 
 if __name__ == "__main__":
