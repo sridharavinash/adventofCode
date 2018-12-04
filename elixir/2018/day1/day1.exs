3d8c63ea4ed525619ee745a610dda78b0e319363
Avinash Sridhar <sridharavinash@github.com>
2018-12-03 21:39:12 -0500

    part2 of day1 elixir

--- elixir/2018/day1/day1.exs
+++ elixir/2018/day1/day1.exs
@@ -1,13 +1,31 @@
 defmodule Day1 do
-    def readFile(filename) do
-        File.stream!(filename)
-            |> Stream.map(fn line ->
-                {integer,_} = Integer.parse(line)
-                integer
-             end)
-            |> Enum.sum()
-    end
+  def part1(filename) do
+    File.stream!(filename)
+    |> Stream.map(fn line ->
+      {integer,_} = Integer.parse(line)
+      integer
+    end)
+    |> Enum.sum()
+  end
+
+  def part2(filename) do
+    File.stream!(filename)
+    |> Stream.map(fn line ->
+      {integer,_} = Integer.parse(line)
+      integer
+    end)
+    |> Stream.cycle()
+    |> Enum.reduce_while({0,%{}}, fn x, {curr, seen} ->
+      new = curr + x
+      if Map.has_key?(seen,new) do
+        {:halt, new}
+      else
+        seen = Map.put(seen,new,true)
+        {:cont, {new, seen}}
+      end
+    end)
+  end
 end
 
-Day1.readFile("input.txt")
-    |> IO.inspect
+Day1.part1("input.txt") |> IO.puts
+Day1.part2("input.txt") |> IO.puts
